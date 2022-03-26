c64aae883f0d7f146ffc908a21669e4196bfae01
quadro <gbds234@gmail.com>
2022-03-26 14:20:47 +0900

    backend: return membersItemList

--- backend/core/core/src/main/java/fleamarket/core/controller/MemberController.java
+++ backend/core/core/src/main/java/fleamarket/core/controller/MemberController.java
@@ -1,14 +1,23 @@
 package fleamarket.core.controller;
 
+import fleamarket.core.DTO.ItemDTO;
+import fleamarket.core.domain.Item;
 import fleamarket.core.domain.Member;
 import fleamarket.core.repository.MemberRepository;
 import fleamarket.core.repository.MemoryMemberRepository;
+import fleamarket.core.web.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +33,18 @@ public class MemberController {
         System.out.println(member.getMemberId());
         memoryMemberRepository.save(member);
         return "ok";
+    }
+
+    @GetMapping("/meber/items")
+    public List<ItemDTO> getSellingItems(HttpServletRequest request){
+        List<ItemDTO> itemDTOs = new ArrayList<>();
+        HttpSession session = request.getSession();
+        if(session == null){
+           return itemDTOs;
+        }
+        Member loggedMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        List<Item> items = loggedMember.getItems();
+        items.stream().forEach(item -> itemDTOs.add(new ItemDTO(item.getItemId(),item.getItemName(),item.getPrice())));
+        return itemDTOs;
     }
 }
