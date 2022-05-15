b4a797b8fe3df4b7cff8c36cbfc57c47957f21a3
quadro <gbds234@gmail.com>
2022-05-15 22:01:01 +0900

    backend: bug fix

--- backend/core/core/src/main/java/fleamarket/core/service/MarketService.java
+++ backend/core/core/src/main/java/fleamarket/core/service/MarketService.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -185,6 +186,8 @@ public class MarketService {
             return "No member there";
         }
         item.setConfirmedMember(reserveMember);
+        System.out.println(item.getConfirmedMember().getName());
+        itemRepository.save(item);
         return "OK";
     }
 
